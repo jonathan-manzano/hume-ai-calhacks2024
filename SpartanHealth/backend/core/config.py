3bf0901f7ba79896648c3dba65c7efe39c32e95d
Jonathan Manzano <jonvincruman@gmail.com>
2024-06-23 11:45:14 -0700

    Updated backend/core/config file with Pinecone API key

--- SpartanHealth/backend/core/config.py
+++ SpartanHealth/backend/core/config.py
@@ -6,6 +6,9 @@ load_dotenv()  # Load environment variables from .env file
 
 
 class Settings:
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    # PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+    # PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
     HUME_API_URL = "https://api.hume.ai/v1/analyze"
     HUME_API_KEY = os.getenv("HUME_API_KEY")
     HUME_SECRET_KEY = os.getenv("HUME_SECRET_KEY")
